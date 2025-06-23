3252a5a18e749e2282da9e667c3f87f9dd45c8a3
VusalSoltanov123 <vusal.soltanov1993@gmail.com>
2025-06-23 16:25:23 +0400

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,19 +1,15 @@
 const ipCache = {};
-
 export default function handler(req, res) {
   const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
   const maxIps = parseInt(process.env.MAX_ALLOWED_IPS || "2");
   const fileUrl = process.env.M3U_URL;
-
+  console.log("📦 M3U_URL içindəki link:", fileUrl);
   if (!fileUrl) return res.status(500).send("Link tapılmadı.");
-
   const ipKey = "global";
   ipCache[ipKey] = ipCache[ipKey] || new Set();
   ipCache[ipKey].add(clientIp);
-
   if (ipCache[ipKey].size > maxIps) {
     return res.status(403).send("Limit keçildi. Link artıq başqa cihazda istifadə olunub.");
   }
-
   res.redirect(fileUrl);
 }
